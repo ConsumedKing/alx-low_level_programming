8ad2582291c6dcef8563d2f5f24e07a4ecebe118
Mustafa Nasser <nsar90554046@gmail.com>
2023-06-15 04:49:27 -0700

    Need Peace in My LIFE gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ' a program that outputs silly string '
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char s[100] = "\"Programming is like building a multilingual puzzle\n";
+	char s[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(s);
 	return (0);
