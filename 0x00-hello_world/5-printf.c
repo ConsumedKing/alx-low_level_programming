0d4c71bc91cab8b4175d2ebd735165dffa988678
Mustafa Nasser <nsar90554046@gmail.com>
2023-06-15 04:55:38 -0700

    printf all day baby

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ' a program that outputs silly string '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[100] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n",s);
+	return (0);
+}
