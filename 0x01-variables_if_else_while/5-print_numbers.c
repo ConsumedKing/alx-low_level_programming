521c54d81441cdd0d178ff5c60d6d791c732b8a7
Mustafa Nasser <nsar90554046@gmail.com>
2023-06-17 02:25:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Descerption: where program starts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
