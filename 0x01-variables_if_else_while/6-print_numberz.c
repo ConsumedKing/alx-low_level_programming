80c18cf2c0af3d9a35353471a0020380ddcc57d7
Mustafa Nasser <nsar90554046@gmail.com>
2023-06-17 02:33:57 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Descerption: where program starts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
