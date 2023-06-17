16a9d2a1e5c86511d805085761429d263b25563e
Mustafa Nasser <nsar90554046@gmail.com>
2023-06-17 01:56:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Descerption: where program starts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	
+	for(a = 'a'; a < 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
