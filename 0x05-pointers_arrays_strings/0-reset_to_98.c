07511e41d3701ac2089714b59758305e5fc3904f
cyberman <mustafanasser@ALX>
2023-06-26 17:47:29 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,10 @@
 /**
  * reset_to_98 - change a pointer value to int 98
  * Descreption: reset_to_98
+ * @n: pointer to a int
  * Return: 0 (Success)
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
