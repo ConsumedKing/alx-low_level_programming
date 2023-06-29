058dedf70431e2d5b9778e249756176532de5cd0
cyberman <mustafanasser@ALX>
2023-06-29 12:36:46 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,28 @@
 #include "main.h"
+#include "string.h"
 /**
  * char *_strncpy- concatinating strings
  * Descreption: adds src to dest overriding \0 byte in dest
  * @dest: pointer to char(s) aka string
  * @src: pointer to char(s) aka string (source)
+ * @n: maximum number of bytes (chars) to be copied
  * Return: 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int j;
+	int len = strlen(src);
 
+	for (j = 0; j < n; j++)
+	{
+		if (j > len || src[j] == '\0')
+		{
+			dest[j] = '\0';
+			continue;
+		}
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+
+	return (dest);
 }
