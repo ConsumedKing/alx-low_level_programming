420f92f743a5bc8d1a065bd2ee56d71b496d1b3a
cyberman <mustafanasser@ALX>
2023-06-29 12:55:37 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, diff = 0;
 
-	while(!diff)
+	while (!diff)
 	{
-		if (s1[i] == '\0' && s2[i]=='\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
 		diff = s1[i] - s2[i];
 		i++;
