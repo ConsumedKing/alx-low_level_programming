907f74de30ccb81e22e7ee38373307b03f3f007a
cyberman <mustafanasser@ALX>
2023-07-03 12:13:41 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * *_memset - set memory location to char
+ * @s: pointer to the location (target).
+ * @b: the car to be set
+ * @n: number of bytes to copy
+ * Return: the pointer to the encoded string.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s+i) = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
