bb1d41ca540a7a525edc3551670ca46a4edbff09
cyberman <mustafanasser@ALX>
2023-07-08 00:42:04 +0300

    0x09

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - print string and then newline
+ * Descreption: print string and then newline
+ * @str: your typical string
+ * Return: 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
