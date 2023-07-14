8006c168a1544159f302d501cd6333bc60ef5c4a
cyberman <mustafanasser@ALX>
2023-07-14 20:56:35 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n >= strlen(s2))
+	if (n > strlen(s2))
 		lenOut = strlen(s1) + strlen(s2) + 1;
 	else
 		lenOut = strlen(s1) + n + 1;
