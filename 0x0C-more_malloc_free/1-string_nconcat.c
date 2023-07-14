3a157b571c59b5e44992961c49d786c20b7e57c8
cyberman <mustafanasser@ALX>
2023-07-14 21:08:48 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; i < lenOut-1; i++)
+	for (i = 0; i < lenOut - 1; i++)
 	{
 		if (i < strlen(s1))
 			p[i] = s1[i];
