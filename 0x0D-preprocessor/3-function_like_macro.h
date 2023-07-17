2dd88cd7a278aed64cd7956051d5fdf0b9c0e15d
cyberman <mustafanasser@ALX>
2023-07-17 21:46:58 +0300

    0-object_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef MACRO_H
-#define MACRO_H
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-#define ABS(X) ((X) < 0 ? (-X) : (X))
+#define ABS(X) ((X) < 0 ? ((X) * (-1)) : (X))
 
 #endif
