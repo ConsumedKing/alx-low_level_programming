093533a32d190e4b0fb8ded687ecfae8d9a8c913
cyberboy <cyberboy@alx.com>
2023-07-19 15:31:08 +0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - a fucntion that prints names
+ * @name: a string to output
+ * @f: a function helper
+ * Return: Void Always
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef POINT_H
+#define POINT_H
+void print_name(char *name, void (*f)(char *));
+#endif
