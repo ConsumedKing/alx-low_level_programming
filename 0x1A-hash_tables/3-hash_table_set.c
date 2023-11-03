46f2a9af98504700d8490094e2f2c75b6ef422b3
Mustafa-cpp <mustafa_165953@fci.sohag.edu.eg>
2023-11-03 20:32:36 +0200

    Fuck

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,68 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * make_node - makes a hash node
+ * @key: key
+ * @value: value
+ * Return: a node of hash table, NULL if something wrong happens
+ */
+hash_node_t *make_node(char * key, char * value)
+{
+	hash_node_t *ptr;
+	
+	ptr = malloc(sizeof(hash_node_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->key = strdup(key);
+	if (ptr->key == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->value = strdup(value);
+	if (ptr->value == NULL)
+	{
+		free(ptr->key);
+		free(ptr);
+		return (NULL);
+	}
+	ptr->next = NULL;
+	return ptr;
+}
+
+
+/**
+ * hash_table_set - set a value to the hash
+ * @ht: hash table
+ * @key: - element to look by
+ * @value: - what to store
+ * Return: 1 if success, 0 otherise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int dig;
+	hash_node_t *holder, *tmp;
+
+	dig = key_index((const unsigned char *)key, ht->size);
+	holder = ht->array[dig];
+	while(holder != NULL)
+	{
+		if (strcmp(holder->key, key) == 0)
+		{
+			free(holder->value);
+			holder->value = strdup(value);
+			if (holder->value == NULL)
+				return (0);
+			return (1);
+		}
+		holder = holder->next;
+	}
+	tmp = make_node(strdup(key), strdup(value));
+	if (tmp == NULL)
+		return 0;
+	tmp->next = ht->array[dig];
+	ht->array[dig] = tmp;
+	return (1);
+
+}
